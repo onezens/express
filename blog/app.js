b25d58811b0d7bbb9f2fb33f733076d34691dd21
wackosix <mail@wackosix.cn>
2016-05-31 14:44:12 +0800

    update router index.js

--- blog/app.js
+++ blog/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var settings = require('./settings.js');
-
+var flash = require('connect-flash');
 //var users = require('./routes/users');
 
 //add session
@@ -20,6 +20,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 app.use(session({
     secret: settings.cookieSecret,
@@ -37,7 +38,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+routes(app);
+//app.use('/', routes);
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,42 +1,24 @@
 var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '主页' });
-});
-router.get('/test', function(req, res, next) {
-
-    res.send('Hello world!');
-})
-
-/// ：首页
-///login ：用户登录
-///reg ：用户注册
-///post ：发表文章
-///logout ：登出
-
-router.get('/login', function(req, res, next){
-
-    res.render('login', {title : '登录'});
-
-});
-router.get('/reg', function(req, res, next){
-
-    res.render('reg', {title : '注册'});
-
-});
-
-router.get('/post', function(req, res, next){
-
-    res.render('post', {title : '发表文章'});
-
-});
-
-router.get('/logout', function(req, res, next){
-
-    res.render('logout', {title : '登出'});
-
-});
-
-module.exports = router;
+//var app = express.Router();
+module.exports = function(app) {
+    app.get('/', function (req, res) {
+        res.render('index', { title: '主页' });
+    });
+    app.get('/reg', function (req, res) {
+        res.render('reg', { title: '注册' });
+    });
+    app.post('/reg', function (req, res) {
+    });
+    app.get('/login', function (req, res) {
+        res.render('login', { title: '登录' });
+    });
+    app.post('/login', function (req, res) {
+    });
+    app.get('/post', function (req, res) {
+        res.render('post', { title: '发表' });
+    });
+    app.post('/post', function (req, res) {
+    });
+    app.get('/logout', function (req, res) {
+    });
+};
